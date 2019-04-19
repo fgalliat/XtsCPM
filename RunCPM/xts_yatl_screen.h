700cceb99d68a3d99e0d365ed855da5c472f6cc9
fgalliat <fgalliat@gmail.com>
2019-04-19 22:19:42 +0200

    STARTED to use ILI9341 TFT Screen

--- RunCPM/xts_yatl_screen.h
+++ RunCPM/xts_yatl_screen.h
@@ -6,25 +6,78 @@
  * Xtase - fgalliat @Apr2019
  */
 
+#ifdef USE_BUILTIN_LCD
+
+    #include "SPI.h"
+    #include "ILI9341_t3n.h"
+    // w/ that lib BOTH CS & DC needs to besome CS pins
+    #define _TFT_DC  20
+    #define _TFT_CS  15
+    // #define TFT_SCK 14 // doesn't work !!!!
+    #define TFT_SCK 13
+
+    #define TFT_MOSI 11
+    #define TFT_MISO 12
+    #define TFT_RST  17
+    #define TFT_LED  16
+
+    ILI9341_t3n tft = ILI9341_t3n(_TFT_CS, _TFT_DC, TFT_RST, TFT_MOSI, TFT_SCK, TFT_MISO);
+
+#endif
+
  class Screen {
      public:
        Screen() { ; }
        ~Screen() { ; }
 
        void cls() {
+#ifndef USE_BUILTIN_LCD
            y_dbug( "XTS: Screen cls" );
+#else
+           tft.fillScreen(ILI9341_BLACK);
+           tft.setCursor(0, 0);
+#endif
        }
+
+       void println(const char* str) { this->println( (char*)str ); }
+
        void println(char* str) {
-           y_dbug( "XTS: ->" );
+#ifndef USE_BUILTIN_LCD
+           y_dbug( "XTS: -> \\" );
            y_dbug( str );
-           y_dbug( "XTS: ->" );
+           y_dbug( "XTS: -> /" );
+#else
+           tft.print(str);
+           tft.print('\n');
+#endif
        }
+
+       int getWidth() { return 320; }
+       int getHeight() { return 240; }
+
  };
 
 
  // declared in xts_yatl.h
  void setupArduinoScreen() {
+#ifndef USE_BUILTIN_LCD
       y_dbug("XTS: setupArduinoScreen -> TODO");
+#else
+   // ensure TFT backlight
+   pinMode(TFT_LED, OUTPUT);
+   digitalWrite(TFT_LED, HIGH);    
+
+   tft.begin();
+   tft.setRotation(3);
+
+   tft.fillScreen(ILI9341_BLACK);
+   tft.setTextColor(ILI9341_YELLOW);
+   tft.setTextSize(2);
+   tft.println("Waiting for Arduino Serial Monitor...");
+
+   tft.setTextSize(1);
+   tft.setTextColor(ILI9341_WHITE);
+#endif
  }
 
  Screen screen;
