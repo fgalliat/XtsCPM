6ae4bc7f6e94f0e41eb375f63ca79956c2ab9ed0
fgalliat <fgalliat@gmail.com>
2019-04-28 15:10:01 +0200

    STARTED to ADD code for my T53 & T5K audio tone files

--- RunCPM/xts_yatl_speaker.h
+++ RunCPM/xts_yatl_speaker.h
@@ -77,6 +77,7 @@
       // playTuneString( (char*)"aac#d");
   }
 
+  // to move away...
   char charUpCase(char ch) {
       if ( ch >= 'a' && ch <= 'z' ) {
           return ch - 'a' + 'A';
@@ -137,6 +138,181 @@
     buzzer.noTone(); // MANDATORY for ESP32
   } // end of playTuneStreamSTring
 
+  // #define _MUSIC_TONES_SUPPORT 1
+  #ifdef _MUSIC_TONES_SUPPORT
+    // T5K Format
+    void __playTune(unsigned char* tuneStream, bool btnStop);
+    // T53
+    void __playTuneT53(unsigned char* tuneStream, bool btnStop);
+
+    // 5KB audio buffer
+    #define AUDIO_BUFF_SIZE (5 * 1024)
+    uint8_t audiobuff[AUDIO_BUFF_SIZE];
+    void cleanAudioBuff() { memset(audiobuff, 0x00, AUDIO_BUFF_SIZE); }
+
+    void led3(bool state) { /*mcu.led(0, state);*/ }
+    void led2(bool state) { /*mcu.led(0, state);*/ }
+
+    bool playTuneFromStorage(char* tuneStreamName, int format = AUDIO_FORMAT_T5K, bool btnStop = true) {
+        buzzer.notTone();
+
+        cleanAudioBuff();
+        // led3(true);
+
+        int tlen = strlen(tuneStreamName);
+        if ( tlen < 1 ) {
+            _puts("ERR : NO file provided\n");
+            // led3(false);
+            return false;
+        }
+
+        // make a routine to clean filenames
+
+        // spe DISK for assets : "Z:"
+        // 'cause CP/M supports ONLY up to P:
+        // so "/Z/0/MARIO.T53"
+        static char ftuneStreamName[1+4+(8+1+3)+1];
+        strcpy(ftuneStreamName, "/Z/0/");
+        strcat(ftuneStreamName, tuneStreamName);
+
+        // auto complete filename if ext is not provided
+        // ~experimental
+        char lastCh = tuneStreamName[ tlen-1 ];
+        lastCh = charUpCase(lastCh);
+
+        if ( !(( lastCh == 'K' || lastCh == '3') && (tlen >= 2 && tuneStreamName[ tlen-2 ] == '5') ) ) {
+            if ( format == AUDIO_FORMAT_T5K ) {
+                strcat(ftuneStreamName, ".T5K");
+            } else {
+                strcat(ftuneStreamName, ".T53");
+            }
+        }
+
+        static unsigned char preBuff[2];
+        int n = mcu.getFS()->readBinFile(ftuneStreamName, preBuff, 2);
+        if ( n <= 0 ) {
+            _puts( ftuneStreamName );
+            _puts("\n");
+            _puts("ZIK File not ready\n");
+            return false;
+        }
+
+        int nbNotes = (preBuff[0]<<8)|preBuff[1];
+
+        int fileLen = (nbNotes*sizeof(Note))+2+16+2;
+        if ( format == AUDIO_FORMAT_T53 ) {
+            fileLen = (nbNotes*(3+3+3))+2+16+2;
+        }
+
+        //file.read( audiobuff, fileLen );
+        n = mcu.getFS()->readBinFile(ftuneStreamName, audiobuff, fileLen);
+        // led3(false);
+
+        if ( format == AUDIO_FORMAT_T5K ) {
+            __playTune( audiobuff, btnStop );  
+        } else {
+            __playTuneT53( audiobuff, btnStop );  
+        }
+
+        buzzer.notTone();
+    }
+
+    // where tuneStream is the audio buffer content
+    // T5K audio format
+    void __playTune(unsigned char* tuneStream, bool btnStop = false) {
+        buzzer.noTone();
+
+        static short nbNotes = (*tuneStream++ << 8) | (*tuneStream++);
+        static char songname[16];
+        for(int i=0; i < 16; i++) {
+            songname[i] = *tuneStream++;
+        }
+        short tempoPercent = (*tuneStream++ << 8) | (*tuneStream++);
+
+        //printfln("nbN:%d title:'%s' tmp:%d", nbNotes, (const char*)songname, tempoPercent);
+        _puts("   -= Playing =-\n");
+        _puts( songname );
+        _puts("\n");
+
+        float tempo = (float)tempoPercent / 100.0;
+        // cf a bit too slow (Cf decoding)
+        tempo *= 0.97;
+
+        for (int thisNote = 0; thisNote < nbNotes; thisNote++) {
+            int note = *tuneStream++;
+            short duration = (*tuneStream++ << 8) | (*tuneStream++);
+            // note 0 -> silence
+            if ( note > 0 ) {
+                buzzer.tone(notes[ note-1 ], duration);
+                //   led2( note > 30 );
+                //   led3( note > 36 );
+            }
+
+            // to distinguish the notes, set a minimum time between them.
+            // the note's duration + 10% seems to work well => 1.10:
+            int pauseBetweenNotes = duration * tempo;
+            delay(pauseBetweenNotes);
+            // stop the tone playing:
+            buzzer.noTone();
+
+            if (btnStop && ( anyBtn() || checkbreak() ) ) {
+                return true;
+            }
+        } // end of note loop
+
+        led2(false);
+        led3(false);
+        return true;
+    } // end of play T5K function
+
+    // T53 Format
+    // where tuneStream is the audio buffer content
+    void __playTuneT53(unsigned char* tuneStream, bool btnStop = false) {
+        buzzer.noTone();
+    
+        short nbNotes = (*tuneStream++ << 8) | (*tuneStream++);
+        char songname[16];
+        for(int i=0; i < 16; i++) {
+            songname[i] = *tuneStream++;
+        }
+        short tempoPercent = (*tuneStream++ << 8) | (*tuneStream++);
+
+        //printfln("nbN:%d title:'%s' tmp:%d", nbNotes, (const char*)songname, tempoPercent);
+        if ( true ) {
+            _puts("   -= Playing =-");
+            _puts( songname );
+            _puts("\n");
+        }
+    
+        float tempo = (float)tempoPercent / 100.0;
+        // cf a bit too slow (Cf decoding)
+        tempo *= 0.97;
+
+        short note, duration, wait;
+        for (int thisNote = 0; thisNote < nbNotes; thisNote++) {
+            note = (*tuneStream++ << 8) | (*tuneStream++);
+            duration = (*tuneStream++ << 8) | (*tuneStream++);
+            wait = (*tuneStream++ << 8) | (*tuneStream++);
+            
+            // note 0 -> silence
+            if ( note > 0 ) {
+                buzzer.tone(note, duration);
+            }
+
+            delay(wait*tempo);
+            // stop the tone playing:
+            buzzer.noTone();
+
+            if (btnStop && ( anyBtn() || checkbreak() ) ) {
+                return true;
+            }
+        }
+        return true;
+    } // end of play T53 function
+
+
+
+  #endif
 
 
 
