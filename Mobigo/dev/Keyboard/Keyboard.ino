c19a0b1bea9621da61ef03935cf7f1550c6c6a14
Franck GALLIAT <fgalliat@gmail.com>
2019-05-19 23:44:01 +0200

    IMPROVED Keyboard reading (mobigo)

--- Mobigo/dev/Keyboard/Keyboard.ino
+++ Mobigo/dev/Keyboard/Keyboard.ino
@@ -4,6 +4,8 @@
  * Vtech Mobigo I - Keyboard
  * Control w/ an SX1509
  * 
+ * @ this time : only the key [ ? ] is missing....
+ * 
  * Arduino UNO
  * SDA : A4
  * SCL : A5
@@ -15,16 +17,6 @@
 // SX1509 I2C address (set by ADDR1 and ADDR0 (00 by default):
 const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address
 SX1509 io; // Create an SX1509 object to be used throughout
-
-// #define KEY_ROWS 4 // Number of rows in the keypad matrix
-// #define KEY_COLS 3 // Number of columns in the keypad matrix
-
-// // keyMap maps row/column combinations to characters:
-// char keyMap[KEY_ROWS][KEY_COLS] = {
-// { '1', '2', '3'},
-// { '4', '5', '6'},
-// { '7', '8', '9'},
-// { '*', '0', '#'}};
 
 const byte ARDUINO_INTERRUPT_PIN = 2;
 // NB Vs BeGin
@@ -49,6 +41,141 @@ const byte ARDUINO_INTERRUPT_PIN = 2;
     { 0x00, 0x00, 0x00, 0x00, 0x00, '\n', 0x00, 0x00, 0x00 },
   };
 
+  #define KEYB_BUFF_LEN 16
+  char _keyBuff[KEYB_BUFF_LEN+1];
+  void flushBuffer() {
+    memset(_keyBuff, 0x00, KEYB_BUFF_LEN+1);
+  }
+
+  bool _kbdShift = false;
+  bool _kbdNums  = false;
+  bool _kbdSymbs = false;
+
+  // #define getKeyAddr(row, col) { return (COLS_NB * row) + col; }
+
+  void _activateRow(int row) {
+    io.digitalWrite(ROWS_BG+row, HIGH);
+    delay(2);
+  }
+
+  void _deactivateRow(int row) {
+    io.digitalWrite(ROWS_BG+row, LOW);
+    // delay(2);
+  }
+
+  bool _isColPressed(int col) {
+    return io.digitalRead( COLS_BG+col ) == HIGH;
+  }
+
+  bool _isKeyPressed(int row, int col) {
+    _activateRow(row);
+    bool ok = _isColPressed(col);
+    _deactivateRow(row);
+    return ok;
+  }
+
+  void _deactivateAllRows() {
+    for(int i=0; i < ROWS_NB; i++) { _deactivateRow(i); }
+  }
+
+  long lastPollTime = -1L;
+  bool lastTimeKeyReleased = true;
+
+  void _doPoll() {
+    _deactivateAllRows();
+
+    if ( lastPollTime == -1L ) {
+      lastPollTime = millis();
+    } else {
+      if ( millis() - lastPollTime < 50 ) { return; }
+
+      // filter + auto repeat
+      if ( !lastTimeKeyReleased ) { 
+        lastTimeKeyReleased = ( millis() - lastPollTime > 300 ); 
+        return; 
+      }
+    }
+
+    // read the metas
+    _kbdShift = _isKeyPressed(1,1);
+    _kbdNums  = _isKeyPressed(0,2);
+    _kbdSymbs = _isKeyPressed(0,3);
+
+    bool oneFoundOnKbd = false;
+    bool oneFoundOnRow = false;
+
+    // read char
+    for(int row=0; row < ROWS_NB; row++) {
+      _activateRow(row);
+      oneFoundOnRow = false;
+
+      for(int col=0; col < COLS_NB; col++) {
+        if ( _isColPressed(col) ) {
+          oneFoundOnKbd = true;
+          oneFoundOnRow = true;
+          char ch = getKeychar(row, col);
+          if ( ch != 0x00 ) {
+            int avail = strlen(_keyBuff);
+            if ( avail <= KEYB_BUFF_LEN ) {
+              strcat(_keyBuff, &ch);
+            } else {
+              // Overflow
+            }
+          }
+        }
+      }
+
+      _deactivateRow(row);
+      if ( oneFoundOnRow ) { break; }
+    }
+
+    lastTimeKeyReleased = !oneFoundOnKbd;
+    lastPollTime = millis();
+  }
+
+  // ========================================
+
+  char getKeychar(int row, int col) {
+    // if ( _kbdShift ) { return shiftedMap[row][col]; }
+    return regularMap[row][col];
+  } 
+
+  void pollKbd() {
+    _doPoll();
+  }
+
+  int availableKbd() {
+    return strlen( _keyBuff );
+  }
+
+  int readKbd() {
+    int avail = strlen( _keyBuff );
+    if ( avail <= 0 ) { return -1; }
+
+    char ch = _keyBuff[0];
+    memmove( &_keyBuff[0], &_keyBuff[1], avail-1); // OK that works
+    _keyBuff[avail-1] = 0x00;
+
+    return (int)ch;
+  }
+
+  void setupKbd() {
+    int i=0;
+    for(; i < COLS_NB; i++) {
+      // io.pinMode( i, INPUT_PULLUP );
+      io.pinMode( COLS_BG+i, INPUT );
+    }
+
+    i=0;
+    for(; i < ROWS_NB; i++) {
+      io.pinMode( ROWS_BG+i, OUTPUT );
+      io.digitalWrite(ROWS_BG+i, LOW);
+    }
+
+    delay(300);
+  }
+
+
 void setup() 
 {
   // Serial is used to display the keypad button pressed:
@@ -62,20 +189,7 @@ void setup()
   }
   delay(300);
 
-  int i=0;
-  for(; i < COLS_NB; i++) {
-    // io.pinMode( i, INPUT_PULLUP );
-    io.pinMode( COLS_BG+i, INPUT );
-  }
-
-
-  i=0;
-  for(; i < ROWS_NB; i++) {
-    io.pinMode( ROWS_BG+i, OUTPUT );
-    io.digitalWrite(ROWS_BG+i, LOW);
-  }
-
-  delay(300);
+  setupKbd();
 
   // Set up the Arduino interrupt pin as an input w/ 
   // internal pull-up. (The SX1509 interrupt is active-low.)
@@ -121,11 +235,18 @@ void dispRow(int row) {
 
 void loop() 
 {
+  /*
     disp[16] = 0x00;
-
     for(int row=0; row < ROWS_NB; row++) {
         dispRow(row);
     }
+  */
+  pollKbd();
+
+  if ( availableKbd() > 0 ) {
+    Serial.print( (char)readKbd() );
+  }
+
 
 //   // If the SX1509 INT pin goes low, a keypad button has
 //   // been pressed:
