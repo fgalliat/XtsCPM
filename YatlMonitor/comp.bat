873aa10b807b875a52c1a431f1f9dc1c21edc06b
fgalliat <fgalliat@gmail.com>
2019-06-19 08:43:26 +0200

    IMPROVED TelnetSender (java)

--- YatlMonitor/comp.bat
+++ YatlMonitor/comp.bat
@@ -2,3 +2,4 @@ rem javac -d bin -cp src;libs/jssc.jar src\TerminalFrame.java
 javac -d bin -cp src;libs/jssc.jar src\SimpleSender.java
 javac -d bin -cp src;libs/jssc.jar src\SubMCUTester.java
 javac -d bin -cp src;libs/jssc.jar src\BdosSerialSender.java
+javac -d bin -cp src;libs/jssc.jar src\TelnetSender.java
--- YatlMonitor/src/TelnetSender.java
+++ YatlMonitor/src/TelnetSender.java
@@ -8,24 +8,63 @@ public class TelnetSender {
         // String ip = "192.168.1.32";
         String ip = "192.168.4.1";
         Socket sk = new Socket(ip, 23);
-
-        sk.getOutputStream().write( ((char)27+"r").getBytes());
         BufferedReader inRead = new BufferedReader(new InputStreamReader(sk.getInputStream()));
         String resp;
-        while( (resp = inRead.readLine()) != null && !resp.startsWith("+OK") ) {
-            // ....
-        }
         
-        String filepath = "/C/0/TEST.TXT";
-        sk.getOutputStream().write( (filepath).getBytes()); sk.getOutputStream().fush();
+        System.out.println("Waiting for userPrompt"); 
+        while( (resp = inRead.readLine()) != null && !resp.equals("") ) {
+            System.out.println(">"+resp);
+        }
+        char[] tmp = new char[32];
+        inRead.read(tmp, 0, 32); // reads UserName : 
+        System.out.println("Sending username"); 
+        sk.getOutputStream().write( "root\n".getBytes()); sk.getOutputStream().flush();
+        // while( (resp = inRead.readLine()) != null ) {
+        //     System.out.println(resp);
+        // }
+        inRead.read(tmp, 0, 32); // reads Password : 
+        System.out.println("Sending passw"); 
+        sk.getOutputStream().write( "yatl\n".getBytes()); sk.getOutputStream().flush();
+        // while( (resp = inRead.readLine()) != null ) {
+        //     System.out.println(resp);
+        // }
+
+        System.out.println("Waiting for User Menu"); 
+        while( (resp = inRead.readLine()) != null && !resp.equals("") ) {
+            System.out.println(">"+resp);
+        }
+
+        // telnetd waits for a READY char
+        sk.getOutputStream().write( ("!").getBytes()); sk.getOutputStream().flush();
+
+        System.out.println("Sending control seq."); 
+        // sk.getOutputStream().write( ((char)27+"r").getBytes()); sk.getOutputStream().flush();
+        sk.getOutputStream().write( ("3").getBytes()); sk.getOutputStream().flush();
+        
+        // while( inRead.available() > 0) {inRead.read();} // reads ??? 
+        Zzz(500);
+
+        while( (resp = inRead.readLine()) != null && !resp.equals("") ) {
+            System.out.println(">"+resp);
+        }
+
+        // Zzz(500);
+        
+        System.out.println("Sending file path"); 
+        String filepath = "/C/0/TEST.TXT\n";
+        sk.getOutputStream().write( (filepath).getBytes()); sk.getOutputStream().flush();
         while( (resp = inRead.readLine()) != null && !resp.startsWith("+OK") ) {
         }
+        System.out.println("Sending file size"); 
         long fileSize = 4;
-        sk.getOutputStream().write( (""+fileSize).getBytes()); sk.getOutputStream().fush();
+        sk.getOutputStream().write( (""+fileSize+"\n").getBytes()); sk.getOutputStream().flush();
         while( (resp = inRead.readLine()) != null && !resp.startsWith("+OK") ) {
         }
 
-        sk.getOutputStream().write( ("AbCd").getBytes()); sk.getOutputStream().fush();
+        System.out.println("Sending file content"); 
+        sk.getOutputStream().write( ("AbCd").getBytes()); sk.getOutputStream().flush();
+        while( (resp = inRead.readLine()) != null && !resp.startsWith("+OK") ) {
+        }
 
         System.out.println("Press Enter...");
         new BufferedReader(new InputStreamReader(System.in)).readLine();
@@ -33,5 +72,9 @@ public class TelnetSender {
         sk.close();
     }
 
+    static void Zzz(long time) {
+        try { Thread.sleep(time); }
+        catch(Exception ex) {}
+    }
 
 }
