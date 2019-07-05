c0766e6e9cad7420b2e7b482c38125b33b24299d
fgalliat <fgalliat@gmail.com>
2019-07-05 20:13:51 +0200

    yac on PAK reading (ino)

--- YatlCPM/xts_yatl_dev_screen.cpp
+++ YatlCPM/xts_yatl_dev_screen.cpp
@@ -1067,7 +1067,7 @@
       if ( numInPak < 0 ) { numInPak=0; }
       if ( numInPak > nbImgs ) { numInPak=nbImgs-1; }
 
-      pakFile.seek( numInPak * ( w*h*2 ) );
+      pakFile.seek( numInPak * ( w*h*2 ) ); // beware : seems to be relative ? 
       uint16_t scanLine[w];
       for(int yy=0; yy < h; yy++) {
          int ct = pakFile.read( (uint8_t*)scanLine, w*2 ); // *2 cf U16
